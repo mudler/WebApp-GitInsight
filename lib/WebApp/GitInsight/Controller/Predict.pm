b8fb8aa7dfdcfe69ed58740c5ff36a60a35bc302
mudler <mudler@dark-lab.net>
2014-08-03 12:33:25 +0200

    first commit

---
+++ lib/WebApp/GitInsight/Controller/Predict.pm
@@ -0,0 +1,33 @@
+package WebApp::GitInsight::Controller::Predict;
+
+use Mojo::Base 'Mojolicious::Controller';
+use Mojo::Util qw/ encode decode spurt slurp /;
+
+sub insight {
+    my $self     = shift;
+    my $username = $self->param("username");
+    my $wd=0 ;
+    $wd=1 if $self->param("no_weekdays");
+    my $Insight  = $self->app->insight($username,$wd,$self->param("left_cutoff"),$self->param("cutoff_offset"));
+    $self->stash(
+        username         => $username,
+        from             => $Insight->start_day,
+        to               => $Insight->last_day,
+        prediction_start => $Insight->prediction_start_day,
+        no_weekdays=>$wd
+    );
+
+    $self->render("prediction");
+}
+
+sub get {
+    my $self     = shift;
+    my $username = $self->param("username");
+    return $self->render(
+        data   => slurp 'public/pred/' . $username . ".png",
+        format => 'png'
+    ) if ( -e 'public/pred/' . $username . ".png" );
+    $self->render_not_found and return;
+}
+
+1;
---
+++ lib/WebApp/GitInsight/Plugin/GitInsight.pm
@@ -0,0 +1,38 @@
+package WebApp::GitInsight::Plugin::GitInsight;
+
+use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::Util qw/ encode decode spurt slurp /;
+use Scalar::Util qw(looks_like_number);
+use GitInsight;
+use JSON;
+
+sub register {
+    my ( $self, $app, $conf ) = @_;
+    $app->helper(
+        insight => sub {
+            shift;
+            $_[0] ||= "mudler";
+            my $Insight = GitInsight->new(
+                file_output  => 'public/pred/' . $_[0],
+                no_day_stats => $_[1] |=
+                    0,
+                left_cutoff => !looks_like_number( $_[2] ) ? 0
+                : $_[2] >= 0 ? $_[2]
+                : 0,
+                cutoff_offset => !looks_like_number( $_[3] ) ? 0
+                : $_[3] >= 0 ? $_[3]
+                : 0
+            );
+            $Insight->contrib_calendar( $_[0] );
+            $Insight->process;
+            spurt(
+                encode_json $Insight->{treemap},
+                'public/json/' . $_[0] . ".json"
+            );
+            return $Insight;
+        }
+    );
+}
+
+1;
+
