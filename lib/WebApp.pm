8ff1e58831ffa67e96c1afa07b92cb8a52171f00
mudler <mudler@dark-lab.net>
2014-08-06 11:50:40 +0200

    added jquery

--- lib/WebApp.pm
+++ lib/WebApp.pm
@@ -33,6 +33,7 @@ sub startup {
     # script.js and extern.js are bundled in the app.js asset
     $app->asset(
         'app.js' => 'http://d3js.org/d3.v3.min.js',
+        'http://code.jquery.com/jquery-2.1.1.min.js',
         '/js/circ.js',
         '/js/script.js'
     );
