4fa0a40c7085dd8ebfe1322b85f22179be998765
mudler <mudler@dark-lab.net>
2014-08-07 14:43:18 +0200

    gathering user infos from github api

--- lib/WebApp/GitInsight/Controller/Predict.pm
+++ lib/WebApp/GitInsight/Controller/Predict.pm
@@ -2,6 +2,7 @@ package WebApp::GitInsight::Controller::Predict;
 
 use Mojo::Base 'Mojolicious::Controller';
 use Mojo::Util qw/ encode decode spurt slurp /;
+use JSON;
 
 sub insight {
     my $self     = shift;
@@ -13,10 +14,16 @@ sub insight {
         $self->param("left_cutoff"),
         $self->param("cutoff_offset")
     );
+
+    my $user_data = decode_json $self->ua->get(
+        "https://api.github.com/users/" . $username )->res->body;
+
     $self->stash(
         username         => $username,
         from             => $Insight->start_day,
         to               => $Insight->last_day,
+        profile_page     => $user_data->{html_url},
+        avatar           => $user_data->{avatar_url},
         stats            => $Insight->{stats},
         labels           => $Insight->{steps},
         prediction_start => $Insight->prediction_start_day,
